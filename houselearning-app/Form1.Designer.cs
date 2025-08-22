8c8af8f3ce9d5e801a8078a6c12bcd4191567bf6
1.9.8.2.6 (AJM) <74386189+ajm19826@users.noreply.github.com>
2025-08-22 18:57:28 -0400

    initial commit

--- houselearning-app/Form1.Designer.cs
+++ houselearning-app/Form1.Designer.cs
@@ -28,13 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.houselearningtopbox = new System.Windows.Forms.GroupBox();
+            this.homebtn = new System.Windows.Forms.Button();
+            this.mathbtn = new System.Windows.Forms.Button();
+            this.sciencebtn = new System.Windows.Forms.Button();
+            this.codingbtn = new System.Windows.Forms.Button();
+            this.gamesbtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.houselearningtopbox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // houselearningtopbox
+            // 
+            this.houselearningtopbox.Controls.Add(this.gamesbtn);
+            this.houselearningtopbox.Controls.Add(this.codingbtn);
+            this.houselearningtopbox.Controls.Add(this.sciencebtn);
+            this.houselearningtopbox.Controls.Add(this.mathbtn);
+            this.houselearningtopbox.Controls.Add(this.homebtn);
+            this.houselearningtopbox.Location = new System.Drawing.Point(-4, 3);
+            this.houselearningtopbox.Name = "houselearningtopbox";
+            this.houselearningtopbox.Size = new System.Drawing.Size(806, 76);
+            this.houselearningtopbox.TabIndex = 0;
+            this.houselearningtopbox.TabStop = false;
+            // 
+            // homebtn
+            // 
+            this.homebtn.Location = new System.Drawing.Point(110, 22);
+            this.homebtn.Name = "homebtn";
+            this.homebtn.Size = new System.Drawing.Size(115, 23);
+            this.homebtn.TabIndex = 0;
+            this.homebtn.Text = "Home";
+            this.homebtn.UseVisualStyleBackColor = true;
+            // 
+            // mathbtn
+            // 
+            this.mathbtn.Location = new System.Drawing.Point(225, 22);
+            this.mathbtn.Name = "mathbtn";
+            this.mathbtn.Size = new System.Drawing.Size(115, 23);
+            this.mathbtn.TabIndex = 1;
+            this.mathbtn.Text = "Math Games";
+            this.mathbtn.UseVisualStyleBackColor = true;
+            // 
+            // sciencebtn
+            // 
+            this.sciencebtn.Location = new System.Drawing.Point(340, 22);
+            this.sciencebtn.Name = "sciencebtn";
+            this.sciencebtn.Size = new System.Drawing.Size(115, 23);
+            this.sciencebtn.TabIndex = 2;
+            this.sciencebtn.Text = "Science Fun";
+            this.sciencebtn.UseVisualStyleBackColor = true;
+            // 
+            // codingbtn
+            // 
+            this.codingbtn.Location = new System.Drawing.Point(455, 22);
+            this.codingbtn.Name = "codingbtn";
+            this.codingbtn.Size = new System.Drawing.Size(115, 23);
+            this.codingbtn.TabIndex = 3;
+            this.codingbtn.Text = "Coding Challenges";
+            this.codingbtn.UseVisualStyleBackColor = true;
+            // 
+            // gamesbtn
+            // 
+            this.gamesbtn.Location = new System.Drawing.Point(570, 22);
+            this.gamesbtn.Name = "gamesbtn";
+            this.gamesbtn.Size = new System.Drawing.Size(115, 23);
+            this.gamesbtn.TabIndex = 4;
+            this.gamesbtn.Text = "Games";
+            this.gamesbtn.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 314);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(297, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 314);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Location = new System.Drawing.Point(571, 109);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 314);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::houselearning_app.Properties.Resources.coding_challenges;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(188, 169);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::houselearning_app.Properties.Resources.science_experiments;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(188, 169);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::houselearning_app.Properties.Resources.math_games;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 169);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 52);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sharpen your math skills with \r\nexciting games and puzzles, \r\nand educational les" +
+    "sons.\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Math Games";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play Now";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::houselearning_app.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.houselearningtopbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HouseLearning";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.houselearningtopbox.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox houselearningtopbox;
+        private System.Windows.Forms.Button gamesbtn;
+        private System.Windows.Forms.Button codingbtn;
+        private System.Windows.Forms.Button sciencebtn;
+        private System.Windows.Forms.Button mathbtn;
+        private System.Windows.Forms.Button homebtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
